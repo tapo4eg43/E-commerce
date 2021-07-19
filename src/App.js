60af2df8a86c5d26adf8b5ce689a1d74a7d422bb
tapo4eg43 <Tilper55>
2021-07-19 13:52:26 +0300

    Implemented firebase utils, included ability to store authenticated use into our firestore database.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import SignInAndSingUp from './pages/sign-in-and-sing-up/sign-in-and-sing-up';
-import {auth} from './firebase/firebase-utils';
+import {auth, createUserProfileDocument} from './firebase/firebase-utils';
 
 
 export default class App extends Component {
@@ -21,9 +21,22 @@ export default class App extends Component {
     unsubscribeFromAuth = null
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
-        })
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapShot => {
+                    this.setState ({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    })
+                });
+            }
+            this.setState({currentUser: userAuth});
+        });
+
     }
 
     componentWillUnmount() {
--- src/pages/sign-in-and-sing-up/sign-in-and-sing-up.js
+++ src/pages/sign-in-and-sing-up/sign-in-and-sing-up.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './sign-in-and-sing-up.scss';
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
 const SignInAndSingUp = () => (
     <div className='sign-in-and-sing-up'>
         <SignIn/>
+        <SignUp/>
     </div>
 )
 
